235075c0e8fe36e0dc173072d196a90d0ef18b33
Michael Movsesov <michael.movsesov@gmail.com>
2018-02-11 23:39:52 -0800

    Added scroll animations and fixed cross-browser bugs.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import $ from 'jquery'
 import 'slick-carousel';
+import ScrollReveal from 'scrollreveal';
 import './scss/_main.scss';
+
 
 
 $(document).ready(function(params) {
@@ -25,5 +27,54 @@ $(document).ready(function(params) {
             $('.navigation-overlay').addClass('navigation-overlay--is-open');
             $('html').addClass('no-scroll');
         }
+    });
+
+
+    // Scrolling animation triggers
+    const sr = ScrollReveal();
+
+    sr.reveal('.navbar', {
+        duration: 1000,
+        origin: 'top',
+        scale: 1,
+    });
+
+    sr.reveal('.hero h1', {
+        duration: 1000,
+        origin: 'top',
+        scale: 1,
+        delay: 800
+    });
+
+    sr.reveal('.hero h2', {
+        duration: 1000,
+        origin: 'top',
+        scale: 1,
+        delay: 1200
+    });
+
+    sr.reveal('.about__card', {
+        duration: 1000,
+        origin: 'top',
+        scale: 1,
+    });
+
+    sr.reveal('.about__feature', {
+        duration: 1000,
+        origin: 'bottom',
+        scale: 1,
+    }, 300);
+
+    sr.reveal('.social__col-left', {
+        duration: 1000,
+        origin: 'left',
+        scale: 1,
+    });
+
+    sr.reveal('.social__col-right', {
+        duration: 1000,
+        origin: 'right',
+        scale: 1,
+        delay: 400
     });
 })
